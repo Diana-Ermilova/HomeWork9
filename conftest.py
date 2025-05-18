359d1e9360b6be83d2d9ddbaa97ab46c897f9cd8
diana.ermilova <diana.ermilova.96@gmail.com>
2025-05-18 18:44:31 +0300

    создание ветки для mid

---
+++ conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from selene import browser
+from selenium import webdriver
+
+@pytest.fixture(autouse=True, scope='function')
+def configure_base_browser():
+    browser.config.driver_options = webdriver.ChromeOptions()
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.window_width = 1366
+    browser.config.window_height = 1280
+    browser.config.driver_options.page_load_strategy = 'eager'
+    browser.config.timeout = 20.0
+    yield
+    browser.quit()
+
+import pytest
+from selene import browser
+from selenium import webdriver
+
+
+
+
+
